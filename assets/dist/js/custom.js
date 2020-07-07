9d4b9ae16691cc43e7ec1b99e234cde476103f9d
Peyton Puckett <peyton@Peytons-MBP.attlocal.net>
2020-07-07 10:28:17 -0500

    pp5762 - Initial Commit

---
+++ assets/dist/js/custom.js
@@ -0,0 +1,31 @@
+/*function submitUserInfo() {
+	event.preventDefault();
+	console.log("The user submitted their information!");
+
+	var firstName = document.getElementById("firstName").value;
+	var lastName = document.getElementById("lastName").value;
+	var favoriteActivity = document.getElementById("inputActivity").value;
+	var favoriteClass = $("#inputSubject").val();
+	var codedBefore = $("#codedBefore").val();
+	var howInterested = $("input[name='customRadio']:checked").val();
+
+	console.log("First Name: ", firstName);
+	console.log("Last Name: ", lastName);
+	console.log("Favorite Activity: ", favoriteActivity);
+	console.log("Favorite Class: ", favoriteClass);
+	console.log("Coded Before: ", codedBefore);
+	console.log("Interest Level: ", howInterested);
+
+	$("#submitModal").modal();
+}
+
+
+function postComment() {
+	event.preventDefault();
+	console.log("The user posted a comment!");
+	var userId = $("#username").val();
+	var comment = document.getElementById("postText").value;
+
+	console.log("UserId: ", userId);
+	console.log("Comment: ", comment);
+}*/
